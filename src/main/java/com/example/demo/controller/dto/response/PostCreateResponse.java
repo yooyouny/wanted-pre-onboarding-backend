e8d76d1c6dcce04ea7507b66fb0b11487d80308b
yooyouny <jhjin2u@hanmail.com>
2023-08-04 18:20:06 +0900

    [#4] Post 응답 DTO 추가

--- src/main/java/com/example/demo/controller/dto/response/PostCreateResponse.java
+++ src/main/java/com/example/demo/controller/dto/response/PostCreateResponse.java
@@ -1,2 +1,22 @@
-package com.example.demo.controller.dto.response;public class PostResponse {
+package com.example.demo.controller.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostCreateResponse {
+
+    private Long id;
+    private String title;
+    private String body;
+
+    @Builder
+    private PostCreateResponse(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
 }
