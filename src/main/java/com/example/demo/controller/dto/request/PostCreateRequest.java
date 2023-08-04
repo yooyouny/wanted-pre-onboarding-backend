6b8507d9275e2154ec7510f618325844cdf3d900
yooyouny <jhjin2u@hanmail.com>
2023-08-04 17:09:10 +0900

    [#4] PostCreateRequest 추가

---
+++ src/main/java/com/example/demo/controller/dto/request/PostCreateRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostCreateRequest {
+
+    @NotBlank(message = "게시글 제목은 필수입니다")
+    private String title;
+
+    @NotBlank(message = "게시글 내용은 필수입니다")
+    private String body;
+
+    @Builder
+    private PostCreateRequest(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+}
