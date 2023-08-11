adb59a679a993e4be5b24a456a40c722560d61cc
yooyouny <jhjin2u@hanmail.com>
2023-08-11 11:52:47 +0900

    [#10] PostService 게시물 수정, 삭제, 전체조회 단위테스트 추가

--- src/test/java/com/example/demo/unit/service/PostServiceTest.java
+++ src/test/java/com/example/demo/unit/service/PostServiceTest.java
@@ -1,12 +1,13 @@
 package com.example.demo.unit.service;
 
 import com.example.demo.controller.dto.request.PostCreateRequest;
+import com.example.demo.controller.dto.request.PostModifyRequest;
 import com.example.demo.controller.dto.response.PostCreateResponse;
 import com.example.demo.controller.dto.response.PostReadResponse;
-import com.example.demo.entity.CustomMemberDetails;
 import com.example.demo.entity.MemberEntity;
 import com.example.demo.entity.PostEntity;
 import com.example.demo.exception.ApplicationException;
+import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.service.PostService;
 import org.junit.jupiter.api.Assertions;
@@ -16,21 +17,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
 
     @Mock
     private PostRepository postRepository;
+    @Mock
+    private MemberRepository memberRepository;
 
     @InjectMocks
     private PostService postService;
@@ -43,18 +49,15 @@ class PostServiceTest {
                 .title("test")
                 .body("contents")
                 .build();
+        MemberEntity writer = createMember("email@");
 
-        MemberEntity member = MemberEntity.builder()
-                .email("email@")
-                .password("password")
-                .build();
-
-        PostEntity savedPost = PostEntity.of(requestPost, member);
+        PostEntity savedPost = PostEntity.of(requestPost, writer);
         savedPost.setIdForTest(1L);
         given(postRepository.save(any())).willReturn(savedPost);
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(writer));
 
         //when
-        PostCreateResponse response = postService.create(requestPost, member.getEmail());
+        PostCreateResponse response = postService.create(requestPost, writer.getEmail());
 
         //then
         Assertions.assertEquals(response.getId(), 1L);
@@ -66,35 +69,30 @@ class PostServiceTest {
     @DisplayName("요청한 포스트 아이디로 저장된 포스트를 찾아서 응답객체로 반환한다")
     void findPostById() {
         //given
-        Long findId = 1L;
-        PostEntity post = PostEntity.builder()
-                .title("test1")
-                .body("contents")
-                .build();
-        post.setIdForTest(findId);
+        Long requestId = 1L;
+        MemberEntity writer = createMember("email@");
+        PostEntity post = createPost("test1", writer);
+        post.setIdForTest(requestId);
         given(postRepository.findById(any())).willReturn(Optional.of(post));
 
         //when
-        PostReadResponse response = postService.getPost(findId);
+        PostReadResponse response = postService.getPost(requestId);
 
         //then
-        Assertions.assertEquals(response.getId(), findId);
+        Assertions.assertEquals(response.getId(), requestId);
     }
     @Test
     @DisplayName("찾으려고 하는 포스트의 아이디가 존재하지 않아 예외가 발생한다")
-    void findPostByNotFoundedId(){
+    void findPostByNotFoundId(){
         //given
-        PostEntity post1 = PostEntity.builder()
-                .title("test1")
-                .body("contents")
-                .build();
-        PostEntity post2 = PostEntity.builder()
-                .title("test2")
-                .body("contents")
-                .build();
+        MemberEntity writer = createMember("email@");
+        PostEntity post1 = createPost("test1", writer);
+        PostEntity post2 = createPost("test2", writer);
+
         List<PostEntity> requestList = List.of(post1, post2);
         postRepository.saveAll(requestList);
-        Long requestId = 3L;
+        Long requestId = 4L;
+        given(postRepository.findById(requestId)).willReturn(Optional.empty());
 
         //when
         ApplicationException e = Assertions.assertThrows(ApplicationException.class, () -> postService.getPost(requestId));
@@ -103,5 +101,136 @@ class PostServiceTest {
         Assertions.assertEquals(e.getStatus(), HttpStatus.NOT_FOUND);
     }
 
+    @Test
+    @DisplayName("모든 포스트를 Page 객체에 담아 응답객체로 반환한다")
+    void findByAllPost(){
+        //given
+        MemberEntity writer = createMember("writer@");
+        PostEntity post1 = createPost("test1", writer);
+        PostEntity post2 = createPost("test2", writer);
+        PostEntity post3 = createPost("test3", writer);
+        PostEntity post4 = createPost("test4", writer);
+        PostEntity post5 = createPost("test5", writer);
+        List<PostEntity> entities = List.of(post1, post2, post3, post4, post5);
+        given(postRepository.findAll(any(Pageable.class))).willReturn(new PageImpl<>(entities));
+
+        //when
+        Page<PostReadResponse> responsePage = postService.getAllPost(0, 5);
+        List<PostReadResponse> responses = responsePage.getContent();
+
+        // Then
+        assertThat(responses).hasSize(5)
+                .extracting("title", "email")
+                .containsExactlyInAnyOrder(
+                        tuple("test1", "writer@"),
+                        tuple("test2", "writer@"),
+                        tuple("test3", "writer@"),
+                        tuple("test4", "writer@"),
+                        tuple("test5", "writer@")
+                );
+    }
+
+    @Test
+    @DisplayName("요청한 포스트 아이디로 저장된 포스트를 찾아서 요청한 내용으로 수정한다")
+    void modifyPostByPostId(){
+        //given
+        MemberEntity requestMember = createMember("email@");
+        PostEntity savedPost = createPost("title", requestMember);
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(requestMember));
+        given(postRepository.findById(any())).willReturn(Optional.of(savedPost));
+
+
+        Long requestPostId = 1L;
+        PostModifyRequest request = PostModifyRequest.builder()
+                .title("modify")
+                .body("modified")
+                .build();
+        savedPost.modifyPost(request);
+        savedPost.setIdForTest(requestPostId);
+        given(postRepository.saveAndFlush(any())).willReturn(savedPost);
+
+        //when
+        PostReadResponse response = postService.modify(requestPostId, request, requestMember.getEmail());
+
+        //then
+        Assertions.assertEquals(response.getId(), 1L);
+        Assertions.assertEquals(response.getTitle(), "modify");
+        Assertions.assertEquals(response.getBody(), "modified");
+    }
+
+    @Test
+    @DisplayName("포스트 수정자와 작성자가 일치하지 않아 예외가 발생한다")
+    void modifyPostByNotAccessMember(){
+        //given
+        MemberEntity wrongMember = createMember("test@");
+        MemberEntity writer = createMember("email@");
+        wrongMember.setIdForTest(1L);
+        wrongMember.setIdForTest(2L);
+        PostEntity post = createPost("test1", writer);
+
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(wrongMember));
+        given(postRepository.findById(any())).willReturn(Optional.of(post));
+
+        Long requestPostId = 1L;
+        PostModifyRequest request = PostModifyRequest.builder()
+                .title("modify")
+                .body("modified")
+                .build();
+
+        //when
+        ApplicationException e = Assertions.assertThrows(ApplicationException.class, () ->
+                postService.modify(requestPostId, request, wrongMember.getEmail()));
+
+        //then
+        Assertions.assertEquals(e.getStatus(), HttpStatus.FORBIDDEN);
+    }
+    @Test
+    @DisplayName("요청한 포스트 아이디로 포스트을 찾아 삭제한다")
+    void deleteByPostId(){
+        MemberEntity requestMember = createMember("email@");
+        PostEntity savedPost = createPost("title", requestMember);
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(requestMember));
+        given(postRepository.findById(any())).willReturn(Optional.of(savedPost));
+
+        Long requestPostId = 1L;
+
+        Assertions.assertDoesNotThrow(() -> postService.delete(requestPostId, "email@"));
+    }
+
+    @Test
+    @DisplayName("포스트 삭제 요청자와 작성자가 일치하지 않아 예외가 발생한다")
+    void deletePostByNotAccessMember(){
+        //given
+        MemberEntity requestMember = createMember("test@");
+        MemberEntity writer = createMember("email@");
+        requestMember.setIdForTest(1L);
+        writer.setIdForTest(2L);
+        PostEntity post = createPost("test1", writer);
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(requestMember));
+        given(postRepository.findById(any())).willReturn(Optional.of(post));
+
+        Long requestPostId = 1L;
+
+        //when
+        ApplicationException e = Assertions.assertThrows(ApplicationException.class, () ->
+                postService.delete(requestPostId, requestMember.getEmail()));
+
+        //then
+        Assertions.assertEquals(e.getStatus(), HttpStatus.FORBIDDEN);
+    }
+
+    private MemberEntity createMember(String email){
+        return MemberEntity.builder()
+                .email(email)
+                .password("password")
+                .build();
+    }
+    private PostEntity createPost(String title, MemberEntity member){
+        return PostEntity.builder()
+                .title(title)
+                .body("contents")
+                .member(member)
+                .build();
+    }
 
 }
