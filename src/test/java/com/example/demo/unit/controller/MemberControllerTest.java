9f2a363e7a8aa965e8296de8e62c6c62191dab5c
yooyouny <jhjin2u@hanmail.com>
2023-08-11 12:03:24 +0900

    [#10] MemberControllerTest 필수값 검증 단위테스트 추가

--- src/test/java/com/example/demo/unit/controller/MemberControllerTest.java
+++ src/test/java/com/example/demo/unit/controller/MemberControllerTest.java
@@ -2,24 +2,19 @@ package com.example.demo.unit.controller;
 
 import com.example.demo.controller.MemberRestController;
 import com.example.demo.controller.dto.request.MemberRegisterRequest;
-import com.example.demo.service.MemberService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,7 +32,7 @@ public class MemberControllerTest {
     }
 
     @Test
-    @DisplayName("요청객체로 멤버 회원가입 요청 시 필수값 검증에서 예외 발생")
+    @DisplayName("요청객체로 멤버 회원가입 요청 시 필수값 검증에서 예외가 발생한다")
     void createMemberWithBlankRequest() throws Exception {
         MemberRegisterRequest request = MemberRegisterRequest.builder()
                 .email("")
@@ -51,7 +46,7 @@ public class MemberControllerTest {
                 .andDo(print());
     }
     @Test
-    @DisplayName("요청객체로 멤버 회원가입 요청 시 비밀번호가 8자 이하라 검증에서 예외 발생")
+    @DisplayName("요청객체로 멤버 회원가입 요청 시 비밀번호가 8자 이하라 검증에서 예외가 발생한다")
     void createMemberWithPasswordLessThan8() throws Exception {
         MemberRegisterRequest request = MemberRegisterRequest.builder()
                 .email("email@")
@@ -66,7 +61,7 @@ public class MemberControllerTest {
     }
 
     @Test
-    @DisplayName("요청객체로 멤버 회원가입 요청 시 이메일 문자 검증에서 예외 발생")
+    @DisplayName("요청객체로 멤버 회원가입 요청 시 이메일 문자 검증에서 예외가 발생한다")
     void createMemberWithInvalidEmail() throws Exception {
         MemberRegisterRequest request = MemberRegisterRequest.builder()
                 .email("email")
@@ -79,7 +74,32 @@ public class MemberControllerTest {
                 .andExpect(status().isBadRequest())
                 .andDo(print());
     }
+    @Test
+    @DisplayName("요청객체로 로그인 요청 시 필수값 검증에서 예외가 발생한다")
+    void createLoginWithBlankRequest() throws Exception {
+        MemberRegisterRequest request = MemberRegisterRequest.builder()
+                .email("")
+                .password("")
+                .build();
 
+        mockMvc.perform(post("/api/v1/members/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(request)))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+    @Test
+    @DisplayName("요청객체로 로그인 요청 시 이메일 문자 검증에서 예외가 발생한다")
+    void createLoginWithInvalidEmail() throws Exception {
+        MemberRegisterRequest request = MemberRegisterRequest.builder()
+                .email("email")
+                .password("password")
+                .build();
 
-
+        mockMvc.perform(post("/api/v1/members/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(request)))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
 }
