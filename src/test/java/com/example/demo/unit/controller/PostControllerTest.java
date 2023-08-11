1c726c2c2ce1f21ceab691bd656ec132bd09f11b
yooyouny <jhjin2u@hanmail.com>
2023-08-11 12:02:50 +0900

    [#10] PostControllerTest 필수값 검증 단위테스트 추가

--- src/test/java/com/example/demo/unit/controller/PostControllerTest.java
+++ src/test/java/com/example/demo/unit/controller/PostControllerTest.java
@@ -11,20 +11,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.IOException;
-
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -41,9 +34,8 @@ public class PostControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(postRestController).build();
         objectMapper = new ObjectMapper();
     }
-
     @Test
-    @DisplayName("요청객체로 포스트 생성 요청 시 valid exception 반환")
+    @DisplayName("요청객체로 포스트 생성 요청 시 필수값 검증에서 예외가 발생한다")
     void createPostWithBlankRequest() throws Exception {
         //given
         PostCreateRequest request = PostCreateRequest.builder()
@@ -52,6 +44,21 @@ public class PostControllerTest {
                 .build();
 
         mockMvc.perform(post("/api/v1/posts")
+                        .content(objectMapper.writeValueAsBytes(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+    @Test
+    @DisplayName("요청객체로 포스트 수정 요청 시 필수값 검증에서 예외가 발생한다")
+    void ModifyPostWithBlankRequest() throws Exception {
+        //given
+        PostCreateRequest request = PostCreateRequest.builder()
+                .title("")
+                .body("body")
+                .build();
+
+        mockMvc.perform(put("/api/v1/posts")
                         .content(objectMapper.writeValueAsBytes(request))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is4xxClientError())
